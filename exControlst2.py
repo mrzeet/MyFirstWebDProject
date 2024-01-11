5b7cdcc47980b519fb4bb3039c0d0fcdfe45cb44
mrzeet <156196757+mrzeet@users.noreply.github.com>
2024-01-11 17:34:50 +0530

    Add files via upload

---
+++ exControlst2.py
@@ -0,0 +1,13 @@
+# num=int(input("Enter a number :"))
+# if num%2==0:
+#     pass
+# else:
+#     print(num,"is odd number !!")
+
+stock=10
+no=int(input("Enter no of iphone want to purchase:"))
+for i in range(nol):
+    if nol<=stock:
+        print("order accepted!!")
+    else:
+        print(i,"out of stock!!!")    
